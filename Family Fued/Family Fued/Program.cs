5e644564d093781bd3dacfa045dbb4d83baea233
unknown <anthonymellon@yahoo.com>
2019-05-14 16:50:42 +1200

    Added comments that have all the instructions for what I need and some methods to start working on

--- Family Fued/Family Fued/Program.cs
+++ Family Fued/Family Fued/Program.cs
@@ -4,10 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* ---Instructions---
+ *
+ *A menu system that:
+ *  -Loads a specefic text file and lists all the contestants on the screen sorted by last name. 43 people are in the file
+ *  -Locate a player in the list and change their interest field, save this back to the text file
+ *  -Generate 10 finalists, no duplicates are allowed
+ *  -Randomly choose one of these finalists to be the player
+ *  -Finish the game
+ *  
+ *The minimum requirements are:
+ *  -Three questions
+ *  -100 people have been surveyed and given their answers to the survey questions.
+ *   The player has to try and guess the top three answers, each answer is worth the number
+ *   of responses it got from the survey. The player is only allowed to give three answers.
+ *   After each question show the correct answers, incorrect answers score zero.
+ *  
+ *External documentation required:
+ *  -An activity diagram of one of your methods (this method must contain at least two structures)
+ *  -An activity diagram of the method that contains the menu system
+ *  -A list of extra features and any bugs
+ *  -A printout of the code
+ *
+ * The program must use methods and an array of structs, it must contain a menu system and the program
+ * must include internal documentation (comments)
+ * 
+ */
+
 namespace Family_Fued
 {
     class Program
     {
+        static void Menu()
+        {
+
+        }
+
+        static void Settings()
+        {
+
+        }
+
+        static void InterestChange()
+        {
+
+        }
+
+
+
         static void Main()
         {
 
